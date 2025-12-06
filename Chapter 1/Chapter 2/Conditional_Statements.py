35be76d9af0f3362f028e35145c09241d15b68fa
indrajitkumar23541-a11y <indrajitkumar23541@gmail.com>
2025-12-07 00:58:06 +0530

    Upload Conditional Statements Program

---
+++ Chapter 1/Chapter 2/Conditional_Statements.py
@@ -0,0 +1,9 @@
+Marks = int(input("Enter Marks : "))
+if(Marks>=90):
+    print("Grade A ")
+elif(Marks>=80 and Marks<90):
+    print("Grade B")
+elif(Marks>=70 and Marks<80):
+    print("Grade C")
+else:
+    print("Grade D")
