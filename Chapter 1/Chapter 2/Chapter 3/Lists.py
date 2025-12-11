b4c68bae9008baa0eac4f7684007f647678292e5
indrajitkumar23541-a11y <indrajitkumar23541@gmail.com>
2025-12-11 23:40:53 +0530

    Upload lists Program

---
+++ Chapter 1/Chapter 2/Chapter 3/Lists.py
@@ -0,0 +1,19 @@
+#students = ["Indrajit", "Mehul", "Himanshu", 34, 56, 43,]
+lists = [23, 45 ,76, 43]
+# print(students)
+# print(type(students))
+# print(students[4])
+# print(len(students))
+# print(len(students[1:]))
+# students.append("Harsh")
+# print(students.sort)
+# lists.append(23)
+# lists.sort()
+#lists.sort(reverse=True)
+#lists.reverse()
+#lists.insert(3,49)
+# lists.remove(45)
+lists.pop(2)
+print(lists)
+
+
