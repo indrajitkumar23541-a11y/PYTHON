641c760db3d604f466326834d7cc26a6d384de57
indrajitkumar23541-a11y <indrajitkumar23541@gmail.com>
2025-12-12 09:31:19 +0530

    Upload set program

---
+++ Chapter 1/Chapter 2/Chapter 3/Chapter 4/Sets.py
@@ -0,0 +1,11 @@
+Nums = {12, 34, 56, 56, 23}
+Nums2 = {23, 12, 56, 23, 45}
+#Nums.add(45)
+#Nums.remove(12)
+#Nums.clear()
+#Nums.pop()
+#Nums.union(Nums2)
+Nums.intersection(Nums2)
+print(Nums)
+print(type(Nums))
+
