13c7ffa7ec9ea8ef0ce2f6d98a4e71f53dc7a9cf
indrajitkumar23541-a11y <indrajitkumar23541@gmail.com>
2025-12-12 10:18:10 +0530

    Write a program to ask the user to enter names of 3movies & store them of list

---
+++ Chapter 1/Chapter 2/Chapter 3/Practice _2.py
@@ -0,0 +1,12 @@
+Movies = []
+Movie1 = input("Enter Movie 1st : ")
+Movies.append(Movie1)
+
+Movie2 = input("Enter Movie 2nd : ")
+Movies.append(Movie2)
+
+Movie3 = input("Enter Movie 3rd : ")
+Movies.append(Movie3)
+
+print(Movies)
+print(type(Movies))
